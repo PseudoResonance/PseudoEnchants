5252f604508362d2b8d6ba8754bd5bc835185789
PseudoResonance <kaio11602@gmail.com>
2019-08-07 23:00:11 -0700

    Update 0.2.2
    Corrected heads

--- src/main/java/io/github/pseudoresonance/pseudoenchants/listeners/BlockL.java
+++ src/main/java/io/github/pseudoresonance/pseudoenchants/listeners/BlockL.java
@@ -14,7 +14,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -28,67 +27,19 @@ import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 
+import io.github.pseudoresonance.pseudoapi.bukkit.PseudoAPI;
 import io.github.pseudoresonance.pseudoapi.bukkit.utils.HeadUtils;
 import io.github.pseudoresonance.pseudoenchants.Config;
 import io.github.pseudoresonance.pseudoenchants.PseudoEnchants;
 import io.github.pseudoresonance.pseudoenchants.enchantments.PseudoEnchantment;
 
 public class BlockL implements Listener {
-	
-	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent e) {
-		ItemStack is = e.getItemInHand();
-		Block b = e.getBlock();
-		if (is.getType() == Material.PLAYER_HEAD) {
-			ItemMeta im = is.getItemMeta();
-			String name = im.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "HeadName"), PersistentDataType.STRING);
-			if (name != null) {
-				String uuid = im.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "Uuid"), PersistentDataType.STRING);
-				if (uuid == null) {
-					String base64 = im.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "Base64"), PersistentDataType.STRING);
-					if (base64 != null) {
-						Skull skull = (Skull) b.getState();
-						skull.getPersistentDataContainer().set(new NamespacedKey(PseudoEnchants.plugin, "HeadName"), PersistentDataType.STRING, name);
-						skull.getPersistentDataContainer().set(new NamespacedKey(PseudoEnchants.plugin, "Base64"), PersistentDataType.STRING, base64);
-						skull.update();
-					}
-				} else {
-					Skull skull = (Skull) b.getState();
-					skull.getPersistentDataContainer().set(new NamespacedKey(PseudoEnchants.plugin, "HeadName"), PersistentDataType.STRING, name);
-					skull.getPersistentDataContainer().set(new NamespacedKey(PseudoEnchants.plugin, "Uuid"), PersistentDataType.STRING, uuid);
-					skull.update();
-				}
-			}
-		}
-	}
 
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
 		Block b = e.getBlock();
 		Player p = e.getPlayer();
 		ItemStack is = p.getInventory().getItemInMainHand();
-		if (p.getGameMode() == GameMode.SURVIVAL || p.getGameMode() == GameMode.ADVENTURE) {
-			if (b.getType() == Material.PLAYER_HEAD || b.getType() == Material.PLAYER_WALL_HEAD) {
-				Skull skull = (Skull) b.getState();
-				String name = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "HeadName"), PersistentDataType.STRING);
-				if (name != null) {
-					ItemStack drop = null;
-					String uuid = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "Uuid"), PersistentDataType.STRING);
-					if (uuid == null) {
-						String base64 = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "Base64"), PersistentDataType.STRING);
-						if (base64 != null) {
-							drop = HeadUtils.getHeadWithBase64(base64, name);
-						}
-					} else {
-						drop = HeadUtils.getHeadWithUUID(UUID.fromString(uuid), name);
-					}
-					if (drop != null) {
-						b.getWorld().dropItemNaturally(b.getLocation(), drop);
-						e.setDropItems(false);
-					}
-				}
-			}
-		}
 		if (is.getAmount() > 0) {
 			if (is.hasItemMeta()) {
 				ItemMeta im = is.getItemMeta();
@@ -252,12 +203,12 @@ public class BlockL implements Listener {
 			}
 			if (b.getType() == Material.PLAYER_HEAD || b.getType() == Material.PLAYER_WALL_HEAD) {
 				Skull skull = (Skull) b.getState();
-				String name = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "HeadName"), PersistentDataType.STRING);
+				String name = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoAPI.plugin, "HeadName"), PersistentDataType.STRING);
 				if (name != null) {
 					ItemStack drop = null;
-					String uuid = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "Uuid"), PersistentDataType.STRING);
+					String uuid = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoAPI.plugin, "Uuid"), PersistentDataType.STRING);
 					if (uuid == null) {
-						String base64 = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoEnchants.plugin, "Base64"), PersistentDataType.STRING);
+						String base64 = skull.getPersistentDataContainer().get(new NamespacedKey(PseudoAPI.plugin, "Base64"), PersistentDataType.STRING);
 						if (base64 != null) {
 							drop = HeadUtils.getHeadWithBase64(base64, name);
 						}
